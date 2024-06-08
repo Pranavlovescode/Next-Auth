10028f04da535456d46250c06e42f134fefb3fce
Pranav Titambe <141265654+Pranavlovescode@users.noreply.github.com>
2024-06-08 18:17:05 +0530

    created database model

--- model/dbModel.ts
+++ model/dbModel.ts
@@ -1,6 +1,33 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema({
+    usename:{
+        type: String,
+        required : [true , "username is required"],
+        unique : true,
+    },
+    email:{
+        type: String,
+        required : [true , "Email is required"],
+        unique : true,
+    },
+    password:{
+        type: String,
+        required : [true , "Password is required"],
+    },
+    isVerified:{
+        type: Boolean,
+        default : false,
+    },
+    isAdmin:{
+        type: Boolean,
+        default : false,
+    },
+    forgetPasswordToken:String,
+    forgetPasswordTokenExpire:Date,  
+    verifiedToken: String,
+    verifiedTokenExpire: Date,
 
 })
 
-module.exports = mongoose.model('test_data', Schema)
+const user = mongoose.models.test_data || mongoose.model('test_data', Schema)
+export default user
