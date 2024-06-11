068517f79621286bbdd090b90f26ffa05db322b1
Pranav Titambe <141265654+Pranavlovescode@users.noreply.github.com>
2024-06-11 17:15:38 +0530

    implemented db connection file

---
+++ db/dbConnect.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const connect_to_db = async()=>{
+    try {
+        mongoose.connect(process.env.MONGO_URI!)
+        const connection  = mongoose.connection
+        connection.on('connected',()=>{
+            console.log("Database connected successfully");            
+        })
+        connection.on('failed',(err)=>{
+            console.log("Database connection failed"+err);
+            process.exit()
+        })
+        
+    } catch (error) {
+        console.log("Something went wrong during connecting database");
+        console.log(error);
+    }
+}
+export default connect_to_db
