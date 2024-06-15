d9d79d6b4f0a7f6b2f3538f7e89f4a1952d7d95d
Pranav Titambe <141265654+Pranavlovescode@users.noreply.github.com>
2024-06-15 17:25:24 +0530

    Done login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,59 @@
-"use client"
+"use client";
 
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { FormEvent, useEffect, useState } from "react";
+type Form = {
+  email: string;
+  password: string;
+};
 export default function Login() {
-  
+  const router = useRouter()
+  const [loading, setLoading]=useState(false)
+  const [formData, setFormData] = useState<Form>({
+    email: "",
+    password: "",
+  });
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    setLoading(true)
+    try {
+      const response = await axios.post("/api/users/login", formData, {
+        withCredentials: true,
+      });
+      if (response.data) {
+        console.log("Login Success",response.data);        
+      }
+      router.push('/')
+    } catch (error: any) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    handleSubmit;
+  }, []);
   return (
     <main className="text-center">
       <h1 className="text-3xl font-bold m-3">Login</h1>
-      <form>
+      <h1>{loading?"Processing":" "}</h1>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" className="mx-2 rounded-sm text-black" />
+        <input
+          id="email"
+          type="email"
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+          className="mx-2 rounded-sm text-black"
+          required
+        />
         <label htmlFor="password">Password</label>
         <input
           id="password"
           type="password"
+          onChange={(e) => {
+            setFormData({ ...formData, password: e.target.value });
+          }}
           className="mx-2 rounded-sm text-black"
+          required
         />
         <button className="p-2 bg-gray-500 rounded-sm">Login</button>
       </form>
